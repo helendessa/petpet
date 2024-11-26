292f0d27494c0aed83abdeb0af69628e6576ff2d
helendessa <helendessa@gmail.com>
2024-11-26 10:26:13 -0300

    Update recommendations: add filters

--- petpet-frontend/src/app/(ui)/recommendations/page.tsx
+++ petpet-frontend/src/app/(ui)/recommendations/page.tsx
@@ -1,8 +1,35 @@
 "use client"
 
+import { useState } from "react";
 import { GeneralHeader } from "@/components/ui/general-header";
 
+// Dados dos posts com categorias
+const posts = [
+    {
+        title: "Clínica Tal Tal",
+        image: "https://www.boanovacj.com.br/images/noticias/6836/6ed29a7c32f51184838eddedab37abdc.JPG",
+        description: "Clínica Tal Tal, oferece os serviços de consultas, exames, cirurgias, internações e vacinas.",
+        address: "Rua da Penha, 290 - Penha II, Passos - MG, 37903-070",
+        phone: "(35) 1234-5678",
+        discount: "PETPET10",
+        mapUrl: "https://maps.app.goo.gl/yGaoDoLgo9kiuCPS9",
+        categories: ["consulta", "exame", "cirurgia", "internação", "vacina"]
+    },
+    {
+        title: "Clínica Tal Tal Teste",
+        image: "https://www.boanovacj.com.br/images/noticias/6836/6ed29a7c32f51184838eddedab37abdc.JPG",
+        description: "Clínica Tal Tal, oferece os serviços de consultas, exames, cirurgias, internações e vacinas.",
+        address: "Rua da Penha, 290 - Penha II, Passos - MG, 37903-070",
+        phone: "(35) 1234-5678",
+        discount: "PETPET10",
+        mapUrl: "https://maps.app.goo.gl/yGaoDoLgo9kiuCPS9",
+        categories: ["vacina", "exame", "cirurgia"]
+    },
+];
+
 export default function Page() {
+    const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+
     const handleShare = (title: string, text: string, url: string) => {
         if (navigator.share) {
             navigator.share({
@@ -23,84 +50,78 @@ export default function Page() {
         }
     };
 
+    const toggleCategory = (category: string) => {
+        setSelectedCategories(prevSelectedCategories =>
+            prevSelectedCategories.includes(category)
+                ? prevSelectedCategories.filter(c => c !== category)
+                : [...prevSelectedCategories, category]
+        );
+    };
+
+    const filteredPosts = selectedCategories.length > 0
+        ? posts.filter(post => selectedCategories.every(category => post.categories.includes(category)))
+        : posts;
+
     return (
         <div>
             <GeneralHeader backHref="/">
                 <div className="font-bold text-lg">Recomendações</div>
             </GeneralHeader> 
-            {/* CLINICA TAL TAL */}
-            <div className="p-6 border b-2 border-yellow-300">
-                <h2 className="text-2xl mb-4">Clínica Tal Tal</h2>
-                <div className="mb-4">
-                    <img src="https://www.boanovacj.com.br/images/noticias/6836/6ed29a7c32f51184838eddedab37abdc.JPG" alt="Placeholder" className="mb-4 w-full h-auto"/>
-                    <p className="text-base">
-                        Clínica Tal Tal, oferece os serviços de consultas, exames, cirurgias, internações e vacinas.<br />
-                        <strong>Endereço:</strong> 
-                            <a href="https://maps.app.goo.gl/yGaoDoLgo9kiuCPS9" target="_blank" rel="noopener noreferrer">
-                            Rua da Penha, 290 - Penha II, Passos - MG, 37903-070
-                            </a><br />
-                        <strong>Telefone:</strong> (35) 1234-5678<br />
-                        <strong>Cupom para desconto de 10%:</strong> PETPET10
-                    </p>
-                    <div className="mt-4">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3731.8843271415135!2d-46.63041542475189!3d-20.714921280857077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b6c3a6a935b78b%3A0x10fd8020560ab6da!2sIFSULDEMINAS%20-%20Campus%20Passos!5e0!3m2!1sen!2sbr!4v1732622348685!5m2!1sen!2sbr"
-                            width="100%"
-                            height="300"
-                            style={{ border: 0 }}
-                            allowFullScreen
-                            loading="lazy"
-                        ></iframe>
-                    </div>
-                    <button
-                        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-                        onClick={() => handleShare(
-                            'Clínica Tal Tal',
-                            'Clínica Tal Tal, oferece os serviços de consultas, exames, cirurgias, internações e vacinas. Endereço: Rua da Penha, 290 - Penha II, Passos - MG, 37903-070. Telefone: (35) 1234-5678. Cupom para desconto de 10%: PETPET10',
-                            'https://maps.app.goo.gl/yGaoDoLgo9kiuCPS9'
-                        )}
-                    >
-                        Compartilhar
-                    </button>
+
+            {/* Interface de seleção de categorias */}
+            <div className="p-6 border-r-2 border-t-2 border-b-2 border-yellow-300">
+                <h2 className="text-xl mb-4">Filtrar por Categoria</h2>
+                <div className="flex flex-wrap gap-2">
+                    {["vacina", "abrigo", "cirurgia", "internação", "consulta", "exame"].map(category => (
+                        <button
+                            key={category}
+                            className={`px-2 py-1 mb-2 rounded ${selectedCategories.includes(category) ? "bg-blue-500 text-yellow-200" : "bg-yellow-400"}`}
+                            onClick={() => toggleCategory(category)}
+                        >
+                            {category}
+                        </button>
+                    ))}
                 </div>
             </div>
 
-            {/* CLINICA TAL TAL */}
-            <div className="p-6 border b-2 border-yellow-300">
-                <h2 className="text-2xl mb-4">Clínica Tal Tal</h2>
-                <div className="mb-4">
-                    <img src="https://www.boanovacj.com.br/images/noticias/6836/6ed29a7c32f51184838eddedab37abdc.JPG" alt="Placeholder" className="mb-4 w-full h-auto"/>
-                    <p className="text-base">
-                        Clínica Tal Tal, oferece os serviços de consultas, exames, cirurgias, internações e vacinas.<br />
-                        <strong>Endereço:</strong> 
-                            <a href="https://maps.app.goo.gl/yGaoDoLgo9kiuCPS9" target="_blank" rel="noopener noreferrer">
-                            Rua da Penha, 290 - Penha II, Passos - MG, 37903-070
+            {/* Exibição dos posts filtrados */}
+            {filteredPosts.map((post, index) => (
+                <div key={index} className="p-6 border-b-2 border-t-2 border-r-2 border-yellow-300 mb-4">
+                    <h2 className="text-2xl mb-4">{post.title}</h2>
+                    <div className="mb-4">
+                        <img src={post.image} alt="Placeholder" className="mb-4 w-full h-auto"/>
+                        <p className="text-base">
+                            {post.description}<br />
+                            <strong>Endereço:</strong> 
+                            <a href={post.mapUrl} target="_blank" rel="noopener noreferrer">
+                                {post.address}
                             </a><br />
-                        <strong>Telefone:</strong> (35) 1234-5678<br />
-                        <strong>Cupom para desconto de 10%:</strong> PETPET10
-                    </p>
-                    <div className="mt-4">
-                        <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3731.8843271415135!2d-46.63041542475189!3d-20.714921280857077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b6c3a6a935b78b%3A0x10fd8020560ab6da!2sIFSULDEMINAS%20-%20Campus%20Passos!5e0!3m2!1sen!2sbr!4v1732622348685!5m2!1sen!2sbr"
-                            width="100%"
-                            height="300"
-                            style={{ border: 0 }}
-                            allowFullScreen
-                            loading="lazy"
-                        ></iframe>
+                            <strong>Telefone:</strong> {post.phone}<br />
+                            <strong>Cupom para desconto de 10%:</strong> {post.discount}
+                        </p>
+                        <div className="mt-4">
+                            <iframe
+                                src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3731.8843271415135!2d-46.63041542475189!3d-20.714921280857077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b6c3a6a935b78b%3A0x10fd8020560ab6da!2sIFSULDEMINAS%20-%20Campus%20Passos!5e0!3m2!1sen!2sbr!4v1732622348685!5m2!1sen!2sbr`}
+                                width="100%"
+                                height="300"
+                                style={{ border: 0 }}
+                                allowFullScreen
+                                loading="lazy"
+                            ></iframe>
+                        </div>
+                        <button
+                            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+                            onClick={() => handleShare(
+                                post.title,
+                                `${post.description} Endereço: ${post.address}. Telefone: ${post.phone}. Cupom para desconto de 10%: ${post.discount}`,
+                                post.mapUrl
+                            )}
+                        >
+                            Compartilhar
+                        </button>
                     </div>
-                    <button
-                        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-                        onClick={() => handleShare(
-                            'Clínica Tal Tal',
-                            'Clínica Tal Tal, oferece os serviços de consultas, exames, cirurgias, internações e vacinas. Endereço: Rua da Penha, 290 - Penha II, Passos - MG, 37903-070. Telefone: (35) 1234-5678. Cupom para desconto de 10%: PETPET10',
-                            'https://maps.app.goo.gl/yGaoDoLgo9kiuCPS9'
-                        )}
-                    >
-                        Compartilhar
-                    </button>
                 </div>
-            </div>
+            ))}
         </div>
     );
 }
