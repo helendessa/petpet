9ff2930c87a0593981cf47f4b554b43e898df2de
helendessa <helendessa@gmail.com>
2024-11-26 07:59:32 -0300

    Update About and Recommendations pages

--- petpet-frontend/src/app/(ui)/recommendations/page.tsx
+++ petpet-frontend/src/app/(ui)/recommendations/page.tsx
@@ -1,5 +1,11 @@
 export default function Page() {
     return (
-        <div>...</div>
+        <div>
+            <header className="flex justify-between p-6 border-b-2 border-yellow-300">
+                <div className="hidden lg:block text-xl">
+                    Recomendações
+                </div>
+            </header>        
+        </div>
     );
 }
